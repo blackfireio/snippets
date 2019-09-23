86bdeff7c4c3a53a8365cd09fc2ca02cb926cf4c
Jérôme Vieilledent <lolautruche@gmail.com>
2019-09-23 16:05:38 +0200

    Added debug_request.php for logging incoming request from Blackfire.io

---
+++ debug_request.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * !!! THIS SCRIPT IS FOR DEBUG PURPOSE ONLY AND COMES WITH NO WARRANTY !!!
+ * !!! USE IT AT YOUR OWN RISK !!!
+ * NOTE THAT IT WILL DUMP YOUR BLACKFIRE CREDENTIALS IN THE LOG FILE.
+ * YOU MAY RESET YOUR BLACKFIRE SERVER CREDENTIALS FROM YOUR BLACKFIRE ACCOUNT AT https://blackfire.io
+ *
+ * DON'T FORGET TO REMOVE THIS FILE ONCE YOUR DEBUG SESSION IS FINISHED.
+ */
+
+// Change the value of this constant to something "really" secret.
+// The value of SECRET will be needed to trigger this script.
+// https://foo.tld/bar?blackfire-debug-secret=<value_of_SECRET>
+const SECRET = 'thisisnotsosecretchangeit';
+// The log file you want to dump the request into.
+$logFile = sys_get_temp_dir().'/blackfire-debug-request.log';
+
+if (!(isset($_GET['blackfire-debug-secret']) && SECRET === $_GET['blackfire-debug-secret'])) {
+    die('KO');
+}
+
+$varsToDump = $_SERVER + [
+        'BLACKFIRE_SERVER_ID' => getenv('BLACKFIRE_SERVER_ID'),
+        'BLACKFIRE_SERVER_TOKEN' => getenv('BLACKFIRE_SERVER_TOKEN'),
+    ];
+error_log(date('Y-m-d h:i:s - ').print_r($varsToDump, true), 3, $logFile);
+
+echo 'OK';
